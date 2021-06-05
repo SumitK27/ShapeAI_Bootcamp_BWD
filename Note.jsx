94fce19deeaa3f3d6706a7551bded049f5b29b05
Sumit Kajbaje <40501698+SumitK27@users.noreply.github.com>
2021-06-05 16:10:55 +0530

    Add files via upload

---
+++ Note.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+function Note() {
+  return (
+    <div className="note">
+      <h1>Web Developer</h1>
+      <p>
+        Want to become a Full-Stack Web Developer and got covered the basic
+        HTML, CSS & JS
+      </p>
+    </div>
+  );
+}
+
+export default Note;
